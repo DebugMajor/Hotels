ec5c0982987171617ec372ffc67e15d8bde790c6
Shashwat Dhondyal <shashwatdhondyal812@gmail.com>
2026-03-09 07:32:28 +0530

    app.js updated

--- app.js
+++ app.js
@@ -112,6 +112,8 @@ app.use((err, req, res, next) => {
 
 
 // Start Server
-app.listen(3000, () => {
-    console.log("App running on port 3000");
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
 });
